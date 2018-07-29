501d7a0967c6418906a2fb202faed1da4ae44ef4
284628487 <284628487@qq.com>
2018-07-29 21:20:25 +0800

    update mediaeffect

---
+++ gles-mediaeffect/src/main/java/com/ccflying/mediaeffect/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ccflying.mediaeffect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaEffectsFragment mFragment = new MediaEffectsFragment();
+        getFragmentManager().beginTransaction().replace(R.id.content, mFragment).commitAllowingStateLoss();
+
+    }
+}
